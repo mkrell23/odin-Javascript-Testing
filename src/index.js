d572f66f9d0ba6998b3061a20377aaaa282a5707
Matt <50344355+mkrell23@users.noreply.github.com>
2025-08-06 13:49:05 -0400

    Capatalize function passes tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const capatalize = {};
+function capatalize(string) {
+    return String(string).charAt(0).toUpperCase() + String(string).slice(1);
+}
 
 
 export {capatalize};
--- test/test.js
+++ test/test.js
@@ -3,3 +3,16 @@ import {capatalize} from '../src/index.js'
 test('it exists', () => {
     expect(capatalize).toBeDefined();
 });
+
+test('it capatalizes a string', () => {
+    const strings = new Map([
+        ["test", "Test"],
+        ["second", "Second"],
+        ["third", "Third"],
+        ["because", "Because"]
+    ]);
+
+    strings.forEach((value, key) => {
+        expect(capatalize(key)).toBe(value);
+    });
+});
